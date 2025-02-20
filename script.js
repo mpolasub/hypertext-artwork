bc9186bb382d6687549565d317a4a07b2511a1bb
mpolasub <nondhawit@gmail.com>
2025-02-20 04:26:20 -0800

    added 3rd page

--- script.js
+++ script.js
@@ -5,52 +5,69 @@ const star3 = document.getElementById('star3');
 const star4 = document.getElementById('star4');
 const star5 = document.getElementById('star5');
 const starContainer = document.getElementsByClassName('star-container');
+let winWidth = window.innerWidth;
+let starPad = parseFloat(getComputedStyle(document.documentElement).fontSize)*2.5 + winWidth/2;
+
+
+window.addEventListener('resize', function() {
+    winWidth = window.innerWidth;
+    starPad = parseFloat(getComputedStyle(document.documentElement).fontSize)*2.5 + winWidth/2;
+  });
+
+
 
 homeLink.addEventListener('mouseenter', () => {
     star.style.filter = 'drop-shadow(0 0 10px rgba(255, 255, 0, 0.7)) brightness(1.1)';
-    star.style.paddingLeft = '0%'
+    star.style.paddingLeft = '70%';
     star.style.transition = 'all 0.3s ease-in-out'
 
     star2.style.filter = 'drop-shadow(0 0 10px rgba(255, 255, 0, 0.7)) brightness(1.1)';
-    star2.style.paddingLeft = '0%'
+    star2.style.paddingLeft = '60%'
     star2.style.transition = 'all 0.3s ease-in-out'
 
     star3.style.filter = 'drop-shadow(0 0 10px rgba(255, 255, 0, 0.7)) brightness(1.1)';
-    star3.style.paddingLeft = '0%'
+    star3.style.paddingLeft = '50%'
     star3.style.transition = 'all 0.3s ease-in-out'
 
     star4.style.filter = 'drop-shadow(0 0 10px rgba(255, 255, 0, 0.7)) brightness(1.1)';
-    star4.style.paddingLeft = '0%'
+    star4.style.paddingLeft = '40%'
     star4.style.transition = 'all 0.3s ease-in-out'
 
     star5.style.filter = 'drop-shadow(0 0 10px rgba(255, 255, 0, 0.7)) brightness(1.1)';
-    star5.style.paddingLeft = '0%'
+    star5.style.paddingLeft = '30%'
     star5.style.transition = 'all 0.3s ease-in-out'
 
     starContainer.margin = '0 auto';
-    starContainer.marginTop = '5%';
+    starContainer.marginTop = '40%';
     starContainer.width = 'fit-content';
 });
 
 homeLink.addEventListener('mouseleave', () => {
     star.style.filter = 'drop-shadow(0 0 0px transparent) brightness(1)';
-    star.style.paddingLeft = '0%';
+    star.style.paddingLeft = '5%';
     star.style.transition = 'all 0.3s ease-in-out';
 
     star2.style.filter = 'drop-shadow(0 0 0px transparent) brightness(1)';
-    star2.style.paddingLeft = '0%';
+    star2.style.paddingLeft = '72%';
     star2.style.transition = 'all 0.3s ease-in-out';
+    star5.style.paddingTop = '10%';
+
 
     star3.style.filter = 'drop-shadow(0 0 0px transparent) brightness(1)';
-    star3.style.paddingLeft = '0%';
+    star3.style.paddingLeft = '18%';
     star3.style.transition = 'all 0.3s ease-in-out';
+    star5.style.paddingTop = '5%';
+
 
     star4.style.filter = 'drop-shadow(0 0 0px transparent) brightness(1)';
-    star4.style.paddingLeft = '0%';
+    star4.style.paddingLeft = '34%';
     star4.style.transition = 'all 0.3s ease-in-out';
+    star5.style.paddingTop = '9%';
+
 
     star5.style.filter = 'drop-shadow(0 0 0px transparent) brightness(1)';
-    star5.style.paddingLeft = '0%';
+    star5.style.paddingLeft = '89%';
+    star5.style.paddingTop = '0%';
     star5.style.transition = 'all 0.3s ease-in-out';
 
     starContainer.marginLeft = '0%';
